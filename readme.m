80fb9936263fead2e4a2aa33691e9410030ec442
zeshan <89.shan@gmail.com>
2014-02-21 20:01:32 -0500

    add an updated readme

--- readme.m
+++
@@ -1,1 +0,0 @@
-These are only 2 of the classes made, the headers of the other classes are included at the top of the source files. But I think this is enough to give you an idea of the scope of the project.
---
+++ readme.md
@@ -0,0 +1,1 @@
+These are just 2 of the classes made, the other classes are included at the top of MegaMan_GC.cpp. I think this is enough of the code to get an idea of the scope of the project.
